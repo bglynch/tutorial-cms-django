0af36c2f5140cfad692f1e67987c8f0a6965fb99
Brian Lynch <bglynch17@gmail.com>
2018-10-31 15:09:49 +0000

    added logout page

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, 'Account created for {}!'.format(username))
-            return redirect('blog-home')
+            messages.success(request, 'Your account has been created! You can now Log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
