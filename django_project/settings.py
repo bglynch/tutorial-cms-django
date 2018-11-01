223b7b4dbbf50db991c93d178f5c506dd8d357ae
Brian Lynch <bglynch17@gmail.com>
2018-11-01 11:02:32 +0000

    updated the uploading of media in settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
